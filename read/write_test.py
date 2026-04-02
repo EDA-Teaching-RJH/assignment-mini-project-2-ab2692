0210e5b0979336b6dd3faddac274af282af98259
Adam Brooker <ab2692@kent.ac.uk>
2026-04-02 17:03:49 +0000

    read testing completed

--- read/write_test.py
+++ read/write_test.py
@@ -1,11 +1,14 @@
-import adam_library
+
 def main():
  
  test_write()
 def test_write():
-    
-    read_name = adam_library.read_last_enrty()
-    assert read_name == "adam"
+    def read_last_enrty():
+     with open ("names_list.txt", "r") as file:   # reads the names_list.txt file
+        last_entry = file.readlines()
+        return last_entry[-1] 
+    read_name = read_last_enrty()
+    assert read_name == "adam\n"
  
  
  
