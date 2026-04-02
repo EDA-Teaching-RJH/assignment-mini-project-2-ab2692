20ac9b52955274596b6efbf393ed2c6e4618b3e8
Adam Brooker <ab2692@kent.ac.uk>
2026-04-02 13:56:11 +0000

    added comments

--- adam_library.py
+++ adam_library.py
@@ -1,10 +1,10 @@
 
-def ask_name():
+def ask_name():  #creates function, askes user name and saves it 
     user_name =input("what is your name?")
     return user_name
 
 
-def say_hello(user_name):
+def say_hello(user_name): # crates function, prints hello and the previously saved user_name
     print("hello", user_name)
 
 
