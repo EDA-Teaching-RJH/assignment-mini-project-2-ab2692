85483bad9ef9d55d68acb662fe35caf7fdec8ba4
Adam Brooker <ab2692@kent.ac.uk>
2026-04-02 16:06:23 +0000

    added main

--- Implamenting libries.py
+++ Implamenting libries.py
@@ -1,18 +1,20 @@
 import adam_library     # imports function 
+def main():
+
+ user_name = adam_library.ask_name()        # uses premade function to save input as user_name  
+
+ adam_library.say_hello(user_name)
+
+ add_to_list = input("do you want to add your name to list? y/n")
+
+ if add_to_list == "y":          # allows the user to refuse data collection 
+     adam_library.save_name(user_name)
+     print("name added to list")
+ else: print("Name not added, have a nice day")
 
 
-user_name = adam_library.ask_name()        # uses premade function to save input as user_name  
 
-adam_library.say_hello(user_name)
+ last_input = adam_library.read_last_enrty()
+ print("last entry:", last_input)
 
-add_to_list = input("do you want to add your name to list? y/n")
-
-if add_to_list == "y":          # allows the user to refuse data collection 
-    adam_library.save_name(user_name)
-    print("name added to list")
-else: print("Name not added, have a nice day")
-
-
-
-last_input = adam_library.read_last_enrty()
-print("last entry:", last_input)
+ main()
