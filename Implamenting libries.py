c44e28a44119cacc8336e4886f6c243fcd07d2a5
Adam Brooker <ab2692@kent.ac.uk>
2026-04-02 16:15:24 +0000

    adding loop functionality so you dont have to keep re running the code to add more names

--- Implamenting libries.py
+++ Implamenting libries.py
@@ -10,13 +10,17 @@ def main():
  if add_to_list == "y":          # allows the user to refuse data collection 
      adam_library.save_name(user_name)
      print("name added to list")
- else: print("Name not added, have a nice day")
+ else: print("Name not added")
 
 
 
  last_input = adam_library.read_last_enrty()
  print("last entry:", last_input)
 
- 
+ ask_again = input("do you want to add another entry? y/n")
+ if ask_again == y:
+   main()
+ else: print("session expired, Have a nice day")  
+
 if __name__ == "__main__":
  main()
