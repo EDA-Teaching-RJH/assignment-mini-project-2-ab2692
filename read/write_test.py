0a4011deb2c76128b892281e79d3162759dad5e2
Adam Brooker <ab2692@kent.ac.uk>
2026-04-02 16:26:33 +0000

    crated testing enviroment

---
+++ read/write_test.py
@@ -0,0 +1,26 @@
+import adam_library     # imports function 
+def main():
+
+ user_name = adam_library.ask_name()        # uses premade function to save input as user_name  
+
+ adam_library.say_hello(user_name)
+
+ add_to_list = input("do you want to add your name to list? y/n")
+
+ if add_to_list == "y":          # allows the user to refuse data collection 
+     adam_library.save_name(user_name)
+     print("name added to list")
+ else: print("Name not added")
+
+
+
+ last_input = adam_library.read_last_enrty()  # calls read function from library 
+ print("last entry:", last_input)             
+
+ ask_again = input("do you want to add another entry? y/n") # loop function asks to add another name and loops back to name entry
+ if ask_again == "y":
+   main()
+ else: print("session expired, Have a nice day")   # if selected anything but "y" code ends
+
+if __name__ == "__main__":
+ main()
